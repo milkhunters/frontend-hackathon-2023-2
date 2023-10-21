c0a3f5ec35715dffc61a3d0e12922f8c72afb55c
iRobbKing <s2068867@iivt.donstu.ru>
2023-10-21 15:40:11 +0300

    test puzzle

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
 <template>
 	<router-view></router-view>
 </template>
+
+<style>
+@import '@/styles/base.css';
+@import '@/styles/style.css';
+</style>
---
+++ src/modules/puzzle/components/test-question.vue
@@ -0,0 +1,39 @@
+<script setup>
+defineProps({
+  id: {
+    type: Number,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  answers: {
+    type: Array,
+    required: true,
+  },
+});
+
+defineEmits(['selected']);
+</script>
+
+<template>
+  <div class="exam_quest">
+    <p class="quest_name">
+      <span class="quest_name_number">{{ id }}.</span>{{ title }}
+    </p>
+    <div class="quest_content">
+      <div class="radio" v-for="answer in answers" :key="answer.id">
+        <input
+          class="custom-radio"
+          type="radio"
+          id="color-1"
+          name="color"
+          @change="$emit('selected', id, answer.id)"
+          :value="answer.value"
+        />
+        <label for="color-1">{{ answer.text }}</label>
+      </div>
+    </div>
+  </div>
+</template>
