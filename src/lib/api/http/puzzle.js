8901e7f9282d0031f5a64c6f7d332446da0d83cc
iRobbKing <s2068867@iivt.donstu.ru>
2023-10-21 16:06:34 +0300

    tests complete

--- src/lib/api/http/puzzle.js
+++ src/lib/api/http/puzzle.js
@@ -1,9 +1,23 @@
 export default {
   getTestPuzzleById(id) {
-    return {};
+    console.log(`Puzzle for id: ${id}.`);
+    return {
+      succeed: true,
+      content: {
+        id: 1,
+        title: 'x',
+        questions: [{ id: 1, text: 'y', value: 123 }],
+      },
+    };
   },
 
-  submitTestPuzzle({ id, responses }) {
+  submitTestPuzzle({ id, answers }) {
+    console.log(`Submitted test with id ${id}`, answers)
     return { succeed: true };
+  },
+
+  submitCodePuzzle({ id, code }) {
+    console.log(`Submitted code with id ${id}`, code);
+    return { succeed: false, content: 'failed to compile' };
   },
 };
--- src/modules/puzzle/pages/test-puzzle-page.vue
+++ src/modules/puzzle/pages/test-puzzle-page.vue
@@ -1,29 +1,36 @@
 <script setup>
-import TestQuestion from '@/modules/puzzle/components/test-question';
+import DefaultLayout from '@/layouts/default-layout.vue';
+import TestQuestion from '@/modules/puzzle/components/test-question.vue';
 import { computed, inject, ref, watch } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import { API_INJECTION_KEY } from '@/keys';
 
 const api = inject(API_INJECTION_KEY);
 const route = useRoute();
 const puzzle = ref(null);
-const responses = ref(null);
+const answers = ref(null);
 
-const selectAnswer = (index, questionId, answerId) => {
-  responses[questionId].responseId = answerId;
+const selectAnswer = (questionId, answerId) => {
+  answers[questionId].answerId = answerId;
 };
 
 const canEndTest = computed(() => {
-  return responses && responses.every(({ responseId }) => responseId);
+  return answers && answers.every(({ answerId }) => answerId);
 });
 
 watch(
   () => route.params.testId,
   async () => {
-    if (route.params.testId) {
-      puzzle.value = await api.puzzle.getTestById(route.params.testId);
-      responses.value = puzzle.value.answers.map((_) => ({ responseId: null }));
-    }
+    if (route.params.testId == null) return;
+
+    const { succeed, content } = await api.puzzle.getTestById(
+      route.params.testId
+    );
+
+    if (!succeed) return;
+
+    puzzle.value = content;
+    answers.value = puzzle.value.questions.map((_) => ({ answerId: null }));
   }
 );
 
@@ -37,51 +44,58 @@ const closeModal = () => {
   submitTestModalOpened.value = false;
 };
 
-const endTest = () => {
+const router = useRouter();
+
+const endTest = async () => {
   if (!canEndTest.value) return;
-  api.puzzle.submitTestPuzzle({
+  const { succeed } = await api.puzzle.submitTestPuzzle({
     id: puzzle.value.id,
-    responses: responses.value,
+    answers: answers.value,
   });
+  if (succeed) await router.push({ name: 'profile' });
 };
 </script>
 
 <template>
-  <template v-if="puzzle">
-    <h1 class="exam_title">puzzle.title</h1>
-    <div class="exam_quest_wrapper">
-      <test-question
-        v-for="(question, index) in puzzle.questions"
-        :key="question.id"
-        :id="index"
-        :title="question.title"
-        :answers="question.answers"
-        @selected="selectAnswer"
-      />
+  <default-layout>
+    <template v-if="puzzle">
+      <h1 class="exam_title">puzzle.title</h1>
+      <div class="exam_quest_wrapper">
+        <test-question
+          v-for="(question, index) in puzzle.questions"
+          :key="question.id"
+          :id="index"
+          :title="question.title"
+          :answers="question.answers"
+          @selected="selectAnswer"
+        />
 
-      <button
-        class="openModalBtn exam_send_btn"
-        :disabled="!canEndTest"
-        @click="openModal"
-      >
-        Отправить результаты
-      </button>
-    </div>
+        <button
+          class="openModalBtn exam_send_btn"
+          :disabled="!canEndTest"
+          @click="openModal"
+        >
+          Отправить результаты
+        </button>
+      </div>
 
-    <Teleport to="body">
-      <div id="myModal" class="modal" v-if="submitTestModalOpened">
-        <div class="modal-content">
-          <p class="modal_title">
-            Вы уверены, что хотите отправить результаты?
-          </p>
-          <div class="modal_buttons">
-            <button class="modal_btn_close" @click="closeModal">Отмена</button>
-            <button class="modal_btn_confirm" @click="endTest">
-              Отправить
-            </button>
+      <Teleport to="body">
+        <div id="myModal" class="modal" v-if="submitTestModalOpened">
+          <div class="modal-content">
+            <p class="modal_title">
+              Вы уверены, что хотите отправить результаты?
+            </p>
+            <div class="modal_buttons">
+              <button class="modal_btn_close" @click="closeModal">
+                Отмена
+              </button>
+              <button class="modal_btn_confirm" @click="endTest">
+                Отправить
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </Teleport>
-  </template>
+      </Teleport>
+    </template>
+  </default-layout>
 </template>
